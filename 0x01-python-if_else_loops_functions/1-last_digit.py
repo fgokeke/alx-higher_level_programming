159f555ad6432886ebf70f921e87da8ce6f663fb
FearGod Okeke <okekefeargod@gmail.com>
2023-09-05 16:35:13 -0700

    a program that prints the ASCII alphabet

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+str_for_6 = "less than 6 and not 0"
 last_digit = number % 10 if number >= 0 else -(-number % 10)
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 "
-        f"and not 0")
+    print(f"Last digit of {number} is {last_digit} and is {str_for_6}")
