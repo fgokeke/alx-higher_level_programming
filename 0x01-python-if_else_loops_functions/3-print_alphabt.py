7200a282474a1848e1b065a42dd3a7e3ece4e969
FearGod Okeke <okekefeargod@gmail.com>
2023-09-05 16:44:28 -0700

    a program that prints the ASCII alphabet with out q and e

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for ascii_value in range(ord('a'), ord('z') + 1):
+    if chr(ascii_value) != 'q' and chr(ascii_value)!= 'e':
+        print("{}".format(chr(ascii_value)), end='')
