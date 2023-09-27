c459c6ce3b210a0ee64182ab6477e1857a4f322e
root <okekefeargod@gmail.com>
2023-09-27 02:15:41 -0700

    a class Square that defines a square by: (based on 1-square.py)3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,8 +3,7 @@
 that defines a square"""
 
 class Square:
-    """a class Square that defines a square by:
-        (based on 1-square.py)"""
+    """a class Square that defines a square."""
 
     def __init__(self, size=0):
         """To initialize a new square.
