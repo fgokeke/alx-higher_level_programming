7da6a68686ed1d8a31680c1377ce882d4a82f02f
root <okekefeargod@gmail.com>
2023-12-11 19:04:52 -0800

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const num = Number(process.argv[2]);
+
+let i;
+
+if (!isNaN(num)) {
+  for (i = 0; i < Math.floor(num); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
