94b439b9cfd880d4b0352bf441c53c9633f5a974
root <okekefeargod@gmail.com>
2023-12-12 15:30:52 -0800

    10. Number conversion2

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -5,12 +5,11 @@ exports.converter = function (base) {
     return;
   }
 
-  const digits = '0123456789abcdefghijklmnopqrstuvwxyz';
   return function convert (number) {
     if (number < base) {
-      return digits[number];
+      return '0123456789abcdefghijklmnopqrstuvwxyz'[number];
     } else {
-      return convert(Math.floor(number / base)) + digits[number % base];
+      return convert(Math.floor(number / base)) + '0123456789abcdefghijklmnopqrstuvwxyz'[number % base];
     }
   };
 };
