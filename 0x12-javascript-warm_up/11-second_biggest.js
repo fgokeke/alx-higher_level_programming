18bd386cb440a588161100b30d39ec2acf42c55d
root <okekefeargod@gmail.com>
2023-12-11 20:15:50 -0800

    11. Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+function findSecondLargest(args) {
+  const integers = args.map(Number);
+
+  if (integers.length < 2) {
+    return 0;
+  }
+
+  let largest = Number.MIN_SAFE_INTEGER;
+  let secondLargest = Number.MIN_SAFE_INTEGER;
+
+  for (const num of integers) {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    }
+    else if (num > secondLargest && num < largest) {
+      secondLargest = num;
+    }
+  }
+  return secondLargest;
+}
+
+const args = process.argv.slice(2);
+
+console.log(findSecondLargest(args));
