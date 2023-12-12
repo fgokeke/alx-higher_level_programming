141d498e511156ad68f74e24ce9a9ecee4df248c
root <okekefeargod@gmail.com>
2023-12-11 18:53:24 -0800

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i;
+
+for (i = 0; i < multiLanguages.length; i++) {
+  console.log(multiLanguages[i]);
+}
