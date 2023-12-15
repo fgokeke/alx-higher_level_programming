c2fb3964f724a4cfaec4520645dc743b00adb07d
root <okekefeargod@gmail.com>
2023-12-15 10:38:21 -0800

    1. Filter states 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""A script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa"""
+"""
+A script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
 
 
 import MySQLdb
@@ -20,7 +22,8 @@ if __name__ == "__main__":
             db=sys.argv[3],
             )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
