b92ea8154900f5039b26dd93f2b1ada545dc73b7
root <okekefeargod@gmail.com>
2023-12-15 10:28:02 -0800

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+    conn.close()
